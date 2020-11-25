fb0d300ea442a286dd15dd2285c8d4ec264fb590
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2020-11-25 21:53:18 +0600

    a fresh start

---
+++ Coding Interview, Prepration Problem and Solution/Program.cs
@@ -0,0 +1,25 @@
+﻿using Coding_Interview__Prepration_Problem_and_Solution.Array_and_String;
+using System;
+
+namespace Coding_Interview__Prepration_Problem_and_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayNstring = new ArrayAndString();
+
+            var printThis = arrayNstring.SpiralOrderMatrix(new int[2, 1]{
+                { 1},
+                { 4}
+            }, 2, 1);
+
+            foreach (var item in printThis)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
